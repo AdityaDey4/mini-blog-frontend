202aacdc55c01116f440a94c69e5b4326bb4e9f4
Aditya Dey <adityadey424@gmail.com>
2025-06-26 15:32:21 +0530

    SS Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import Navbar from "./pages/Navbar";
 
 export default function App() {
   return (
-    <div className="w-screen h-screen flex justify-center items-start bg-gray-50">
+
+    <div className="w-screen min-h-screen flex justify-center items-start bg-gray-50">
       <div className="w-1/2 text-white">
         <Navbar />
         <main className="flex-1 w-full py-6">
@@ -18,13 +19,5 @@ export default function App() {
         </main>
       </div>
     </div>
-
-    // <div className="w-screen h-screen flex justify-center items-start bg-white">
-    //   <div className="w-1/2 bg-black text-white p-4">
-    //     {/* Your content starts from top */}
-    //     <h1 className="text-xl font-bold">Blog Title</h1>
-    //     <p>This content starts from the top but is horizontally centered.</p>
-    //   </div>
-    // </div>
   );
 }
